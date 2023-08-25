06a8c0459c927bd0de1054c97d8ed03989820f84
Treasure <obisiket@yahoo.com>
2023-08-25 15:26:01 +0100

    it

--- components/base/Seo.vue
+++ components/base/Seo.vue
@@ -1,7 +1,18 @@
 <template> 
-  <div>
-    <img :src="data?.agency_wizard.website_details.favIcon" />
+<div class="h-[3rem]"> 
+  &nbsp;
+  <div class="fixed pt-[.4rem] top-0"> 
+    <div class="flex w-full justify-center items-center flex-row">
+      <div class="flex gap-3 items-center"> 
+        Favicon: <span class="inline-block w-[2rem] rounded-lg overflow-hidden bg-slate-300 aspect-square"><img class="w-full h-full object-cover" :src="data?.agency_wizard.website_details.favIcon" /></span>
+      </div>
+      <div>
+        Title: <span> {{ data?.agency_wizard.seo.title }} </span>
+      </div>
+    </div>
   </div>
+
+</div>
 </template>
 
 <script setup lang="ts"> 
